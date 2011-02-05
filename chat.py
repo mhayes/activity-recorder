02f16d7acab76cb3c8a1063fa60d2e4ec0193763
Mark Hayes <mark@deployfx.com>
2011-02-04 16:05:04 -0800

    refactored code, introduced remove activity functionality, and introduced personable messages

--- chat.py
+++ chat.py
@@ -1,37 +1,49 @@
-from google.appengine.api import xmpp
+import logging
 from google.appengine.ext import db
+from google.appengine.api import xmpp
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import xmpp_handlers
+
+# Potential error messages that can be displayed
+ERROR_SAVING= "An error preventing me from saving your activity"
+ERROR_UNAUTHORIZED = "You're not allowed to do that"
+ERROR_UNKNOWN_ID = "I can't seem to locate that record"
 
 class Activity(db.Model):
     user = db.IMProperty()
     summary = db.StringProperty()
     date = db.DateTimeProperty(auto_now_add=True)
 
-class XMPPHandler(webapp.RequestHandler):
-    def post(self):
-        message = xmpp.Message(self.request.POST)
-        cmd = message.body[0:5].lower()
-        if cmd == "/help":
-            message.reply = """
-            Supported commands are:
-            */help* (this help msg)
-            """
+class XMPPHandler(xmpp_handlers.CommandHandler):
+    def rpt_command(self, message=None):
+        """Generate summary of activity."""
+        im_from = db.IM("xmpp", message.sender)
+        message.reply("Right away, check your e-mail in just a minute")
+    def rm_command(self, message=None):
+        """Removes user activity."""
+        im_from = db.IM("xmpp", message.sender)
+        activity_id = int(message.arg)
+        activity = Activity.get_by_id(activity_id)
+        if activity and activity.user == im_from:
+            activity.delete()
+            message.reply("That activity (%d) is gone!" % activity_id)
+        elif activity and activity.user != im_from:
+            message.reply(ERROR_UNAUTHORIZED)
         else:
-            activity = Activity(user = db.IM("xmpp", message.sender), 
-                                    summary = message.body)
-            activity.put()
-            message.reply("recorded")
+            message.reply(ERROR_UNKNOWN_ID)
+    def text_message(self, message=None):
+        """Record activity."""
+        im_from = db.IM("xmpp", message.sender)
+        activity = Activity(user=im_from, summary=message.body)
+        try:
+            key = activity.put()
+            message.reply("Logged as %s" % key.id())
+        except:
+            message.reply(ERROR_SAVING_MSG)
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('deployFX')
-
-        application = webapp.WSGIApplication(
-        [('/', MainPage),
-        ('/_ah/xmpp/message/chat/', XMPPHandler)],
-        debug=True)
+application = webapp.WSGIApplication(
+    [('/_ah/xmpp/message/chat/', XMPPHandler)], debug=True)
 
 def main():
     run_wsgi_app(application)
